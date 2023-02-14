76f92112fc05f09d93aaf6eb8362d257ee624025
Sergey Drepin <sdrepin@mail.ru>
2023-02-14 15:46:17 +0300

    Добавил файл

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿double a;
+double b;
+double c;
+double max;
+a = 22;
+b = 3;
+c = 9;
+
+if (a > b)
+{
+    max = a;
+}
+else
+{
+    max = b;
+}
+
+if (c > max)
+{
+    max = c;
+}
+Console.WriteLine(max);
